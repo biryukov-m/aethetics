0fcc67926a85e1cedf67e040938c81917664667b
Max <biryukov.mm@gmail.com>
2023-02-15 00:09:22 +0200

    feature: component for item in favourites of cabinet

---
+++ app/src/pages/cabinet/CabinetFavouritesItem.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import RatingBar from "../../components/ratingBar/RatingBar";
+import ButtonGreenMini from "../../components/buttonGreenMini/ButtonGreenMini";
+import { Product } from "../../types/types";
+
+const CabinetFavouritesItem: React.FC<Product> = ({
+  imageUrl,
+  imageAlt,
+  rating,
+  availability,
+  name,
+  price,
+}) => (
+  <li className="item">
+    <div className="column for-image">
+      <div className="image-holder">
+        <img src={imageUrl} alt={imageAlt} />
+      </div>
+    </div>
+    <div className="column for-interface">
+      <div className="rating-and-availability">
+        <RatingBar rating={rating} rateable={false} />
+        <p className="availability">
+          {availability ? "В наявності" : "Немає в наявності"}
+        </p>
+      </div>
+      <p className="name">{name}</p>
+      <p className="price">{price} грн</p>
+      <div className="buttons">
+        <ButtonGreenMini text={"В кошик"}></ButtonGreenMini>
+        <a href="#" className="delete">
+          Видалити
+        </a>
+      </div>
+    </div>
+  </li>
+);
+
+export default CabinetFavouritesItem;
