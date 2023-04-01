46d9a854a05c5f50e4a49318974d9f80f0a23189
Maxym Biriukov <biryukov.mm@gmail.com>
2023-04-01 23:09:31 +0300

    ref: Fix go back from span to button

--- app/src/components/goBack/GoBack.tsx
+++ app/src/components/goBack/GoBack.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React from 'react';
 
 const GoBack: React.FC = () => (
-  <span className="go-back" onClick={(e) => window.history.back()} />
+  <button
+    aria-label="go back"
+    type="button"
+    className="go-back"
+    onClick={() => window.history.back()}
+  />
 );
 
 export default GoBack;
