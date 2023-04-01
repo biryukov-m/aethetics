9d9e9f72215903028779201397a8c8edbe6ef3bd
Maxym Biriukov <biryukov.mm@gmail.com>
2023-04-01 22:52:07 +0300

    ref:  Remove redux from app

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,17 +1,13 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-import App from "./App";
-import "./sass/all.scss";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './sass/all.scss';
 
-const container = document.getElementById("wrapper")!;
+const container = document.getElementById('wrapper')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
