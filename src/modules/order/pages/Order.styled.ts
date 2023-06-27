72436eb0b89116c1cd751e92f8a8cc1b9c3e0aff
Maxym Biriukov <biryukov.mm@gmail.com>
2023-06-27 14:17:34 +0300

    feat: Add tabs mechanism to order page

--- src/modules/order/pages/Order.styled.ts
+++ src/modules/order/pages/Order.styled.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { COLORS, MEDIA } from '../../theme';
+import { COLORS, FONTS, MEDIA } from '../../theme';
 
 export const Header = styled.h3`
   text-align: center;
@@ -14,5 +14,41 @@ export const Header = styled.h3`
   @media screen and (max-width: ${MEDIA.mobile}px) {
     font-size: 18px;
     margin: 30px auto 45px;
+  }
+`;
+export const Tabs = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+export const Tab = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  cursor: pointer;
+  &:nth-last-of-type(1) {
+    margin-right: 15px;
+  }
+  span {
+    color: ${COLORS.black};
+    font-size: ${FONTS.SIZES.l};
+    font-family: ${FONTS.FAMILIES.normal};
+  }
+`;
+export const Counter = styled.div`
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  font-family: ${FONTS.FAMILIES.normal};
+  font-size: ${FONTS.SIZES.xl};
+  font-weight: ${FONTS.WEIGHTS.normal};
+  background-color: ${COLORS.white};
+  color: ${COLORS.accent};
+  border: 1px solid ${COLORS.accent};
+  text-align: center;
+  vertical-align: middle;
+  &.active {
+    background-color: ${COLORS.accent};
+    color: ${COLORS.creamy};
   }
 `;
--- src/modules/theme/colors.const.ts
+++ src/modules/theme/colors.const.ts
@@ -2,6 +2,7 @@ export const COLORS = {
   creamy: '#fffbf5',
   accent: '#4b6049',
   text: '#484847',
+  grey_text: '#b3b3b2',
   white: '#ffffff',
   black: '#000000'
 };
