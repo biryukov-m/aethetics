58f5d4c3b09c7a2c8caee911664cbd4c3c5464fb
Maxym Biriukov <biryukov.mm@gmail.com>
2023-06-27 14:17:15 +0300

    feat: Add styles for personal data form in order page

--- src/modules/order/components/OrderPersonalData/OrderPersonalData.styled.ts
+++ src/modules/order/components/OrderPersonalData/OrderPersonalData.styled.ts
@@ -1,3 +1,55 @@
 import styled from 'styled-components';
+import { COLORS, FONTS, MEDIA } from '../../../theme';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  margin-bottom: 50px;
+  form {
+    display: flex;
+    justify-content: space-between;
+    input {
+      font-family: ${FONTS.FAMILIES.normal};
+      font-size: ${FONTS.SIZES.m};
+      line-height: 150%;
+      color: ${COLORS.text};
+      border-radius: 0;
+      border: 1px solid #000;
+      padding: 5px 8px 10px;
+      margin-bottom: 28px;
+      @media (max-width: ${MEDIA.tablet}px) {
+        margin-bottom: 15px;
+        font-size: clamp(${FONTS.SIZES.xs}, 3.3vw, ${FONTS.SIZES.s});
+      }
+      &:nth-last-of-type(1) {
+        margin-bottom: 0;
+      }
+      &::placeholder {
+        font-family: ${FONTS.FAMILIES.normal};
+        font-size: ${FONTS.SIZES.m};
+        line-height: 150%;
+        color: ${COLORS.grey_text};
+      }
+    }
+    label {
+      margin-bottom: 3px;
+      font-family: ${FONTS.FAMILIES.normal};
+      font-size: ${FONTS.SIZES.m};
+      line-height: 150%;
+      color: ${COLORS.text};
+      @media (max-width: ${MEDIA.tablet}px) {
+        font-size: clamp(14px, 3.3vw, 19px);
+        margin-bottom: 0;
+      }
+    }
+  }
+`;
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 370px;
+`;
+
+export const BtnColumn = styled(Column)`
+  align-items: center;
+  justify-content: flex-end;
+`;
--- src/modules/order/components/OrderPersonalData/OrderPersonalData.tsx
+++ src/modules/order/components/OrderPersonalData/OrderPersonalData.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
+
 import { Field, Form, Formik } from 'formik';
 import * as Styled from './OrderPersonalData.styled';
 import { Button } from '../../../common/button/button.styled';
@@ -19,35 +19,39 @@ export const OrderPersonalData: React.FC = () => {
       >
         {({ isSubmitting }) => (
           <Form>
-            <label htmlFor="name">Ваше ім’я</label>
-            <Field type="text" className="name" name="name" id="name" placeholder="Світлана" />
-            <label htmlFor="surname">Ваше прізвище</label>
-            <Field
-              type="text"
-              className="surname"
-              name="surname"
-              id="surname"
-              placeholder="Світлана"
-            />
-            <label htmlFor="email">E-mail</label>
-            <Field
-              type="email"
-              className="email"
-              name="email"
-              id="email"
-              placeholder="shevSv002@gmail.com"
-            />
-            <label htmlFor="phone">Номер телефону</label>
-            <Field
-              type="telephone"
-              className="telephone"
-              id="phone"
-              name="phone"
-              placeholder="+38 068 564 77 99"
-            />
-            <Button type="submit" disabled={isSubmitting}>
-              Далі
-            </Button>
+            <Styled.Column>
+              <label htmlFor="name">Ваше ім’я</label>
+              <Field type="text" className="name" name="name" id="name" placeholder="Світлана" />
+              <label htmlFor="surname">Ваше прізвище</label>
+              <Field
+                type="text"
+                className="surname"
+                name="surname"
+                id="surname"
+                placeholder="Шевченко"
+              />
+              <label htmlFor="email">E-mail</label>
+              <Field
+                type="email"
+                className="email"
+                name="email"
+                id="email"
+                placeholder="shevsv002@gmail.com"
+              />
+              <label htmlFor="phone">Номер телефону</label>
+              <Field
+                type="telephone"
+                className="telephone"
+                id="phone"
+                name="phone"
+                placeholder="+38 068 555 77 99"
+              />
+            </Styled.Column>
+            <Styled.BtnColumn>
+              <Button type="submit" disabled={isSubmitting}>
+                Далі
+              </Button>
+            </Styled.BtnColumn>
           </Form>
         )}
       </Formik>
