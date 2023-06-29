4fcc6bd262ec3f8e48959dfcb1f1e3e556d37081
Maxym Biriukov <biryukov.mm@gmail.com>
2023-06-29 16:08:26 +0300

    feat: Add radio groups to payment and delivery options

--- src/modules/order/components/OrderPaymentAndDelivery/OrderPaymentAndDelivery.tsx
+++ src/modules/order/components/OrderPaymentAndDelivery/OrderPaymentAndDelivery.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 // import * as Styled from './OrderPaymentAndDelivery.styled';
 import { OrderPaymentAndDeliveryNewAddress } from './OrderPaymentAndDeliveryNewAddress';
+import { OrderPaymentAndDeliveryOptions } from './OrderPaymentAndDeliveryOptions';
 
 export const OrderPaymentAndDelivery: React.FC = () => {
   // eslint-disable-next-line no-console
   console.log();
 
-  return <OrderPaymentAndDeliveryNewAddress />;
+  return (
+    <>
+      <OrderPaymentAndDeliveryNewAddress />
+      <OrderPaymentAndDeliveryOptions />
+    </>
+  );
 };
