ab2ed96a52812fa914bac28e646f739821d64ab1
Maxym Biriukov <biryukov.mm@gmail.com>
2023-07-09 23:40:03 +0300

    bug: Fix address validation schema

--- src/schemas/address.schema.ts
+++ src/schemas/address.schema.ts
@@ -4,7 +4,7 @@ export const addressValidationSchema = object().shape({
   city: string().min(4).max(40).required('Назва населеного пункту - обов`язкове поле вводу!'),
   street: string().min(4).max(25).required('Назва вулиці - обов`язкове поле вводу!'),
   house: string().min(1).max(8).required('Номер будинку - обов`язкове поле вводу!'),
-  apartment: string().min(4).max(40),
+  apartment: string().min(1).max(40),
   postal: string()
     .min(4)
     .max(40)
