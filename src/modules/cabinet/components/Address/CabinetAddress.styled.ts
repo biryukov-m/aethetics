305d2346ad284b84764f7655907df58afb209a87
Maxym Biriukov <biryukov.mm@gmail.com>
2023-09-16 20:44:09 +0300

    feat: Add styles for cabinet

---
+++ src/modules/cabinet/components/Address/CabinetAddress.styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+// export const Header = styled.h3``;
+// export const Header = styled.h3``;
+// export const Header = styled.h3``;
+// export const Header = styled.h3``;
+// export const Header = styled.h3``;
+// export const Header = styled.h3``;
+// export const Header = styled.h3``;
+// export const Header = styled.h3``;
