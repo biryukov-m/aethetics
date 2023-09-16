e5f3452048ed2b2d675a9a39b2cbd094e8010a4e
Maxym Biriukov <biryukov.mm@gmail.com>
2023-09-16 20:46:52 +0300

    feat: Implemented tabs navigation logic in cabinet

--- src/modules/cabinet/pages/Cabinet.tsx
+++ src/modules/cabinet/pages/Cabinet.tsx
@@ -3,40 +3,15 @@ import { Outlet } from 'react-router-dom';
 import CabinetTabs from '../components/Tabs/CabinetTabs';
 import * as Styled from './Cabinet.styled';
 
-const Cabinet: React.FC = () => {
-  // eslint-disable-next-line no-console
-  console.log('1');
-  // const handleTabsClick = (event: React.MouseEvent) => {
-  //   const target = event.target as Element;
-  //   if (target.classList.contains('tab-link')) {
-  //     document.querySelector('.tab.active')?.classList.remove('active');
-
-  //     const id = target.getAttribute('data-for');
-  //     if (id) {
-  //       document.getElementById(id)?.classList.add('active');
-  //     }
-
-  //     const links = document.querySelectorAll('.tab-link');
-  //     links?.forEach((link) => {
-  //       if (link.getAttribute('data-for') === id) {
-  //         link.classList.add('active');
-  //       } else {
-  //         link.classList.remove('active');
-  //       }
-  //     });
-  //   }
-  // };
-  return (
-    <Styled.Section>
-      <div className="layout">
-        <Styled.Wrapper>
-          <Styled.Header>Особистий кабінет</Styled.Header>
-          <CabinetTabs />
-          <Outlet />
-        </Styled.Wrapper>
-      </div>
-    </Styled.Section>
-  );
-};
-
+const Cabinet: React.FC = () => (
+  <Styled.Section>
+    <div className="layout">
+      <Styled.Wrapper>
+        <Styled.Header>Особистий кабінет</Styled.Header>
+        <CabinetTabs />
+        <Outlet />
+      </Styled.Wrapper>
+    </div>
+  </Styled.Section>
+);
 export default Cabinet;
