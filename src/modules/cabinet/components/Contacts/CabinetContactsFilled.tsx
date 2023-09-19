0fc015faaf8fdfd18540c747aaa7a1a5589db832
Max <biryukov.mm@gmail.com>
2023-09-19 15:53:40 +0300

    feat: Styles for cabinet contacts

--- src/modules/cabinet/components/Contacts/CabinetContactsFilled.tsx
+++ src/modules/cabinet/components/Contacts/CabinetContactsFilled.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from '../../../common/button/button.styled';
 import * as Styled from './CabinetContactsFilled.styled';
 
 interface IProps {
@@ -10,26 +11,31 @@ interface IProps {
 }
 
 export const CabinetContactsFilled: React.FC<IProps> = ({ name, surname, email, phone, dob }) => (
-  <Styled.Grid>
-    <Styled.Container>
-      <Styled.Label>Ім’я:</Styled.Label>
-      <Styled.Text>{name}</Styled.Text>
-    </Styled.Container>
-    <Styled.Container>
-      <Styled.Label>Прізвище:</Styled.Label>
-      <Styled.Text>{surname}</Styled.Text>
-    </Styled.Container>
-    <Styled.Container>
-      <Styled.Label>E-mail:</Styled.Label>
-      <Styled.Text>{email}</Styled.Text>
-    </Styled.Container>
-    <Styled.Container>
-      <Styled.Label>Номер телефону:</Styled.Label>
-      <Styled.Text>{phone}</Styled.Text>
-    </Styled.Container>
-    <Styled.Container>
-      <Styled.Label>Дата народження:</Styled.Label>
-      <Styled.Text>{dob}</Styled.Text>
-    </Styled.Container>
-  </Styled.Grid>
+  <>
+    <Styled.Grid>
+      <Styled.Container>
+        <Styled.Label>Ім’я:</Styled.Label>
+        <Styled.Text>{name}</Styled.Text>
+      </Styled.Container>
+      <Styled.Container>
+        <Styled.Label>Прізвище:</Styled.Label>
+        <Styled.Text>{surname}</Styled.Text>
+      </Styled.Container>
+      <Styled.Container>
+        <Styled.Label>E-mail:</Styled.Label>
+        <Styled.Text>{email}</Styled.Text>
+      </Styled.Container>
+      <Styled.Container>
+        <Styled.Label>Номер телефону:</Styled.Label>
+        <Styled.Text>{phone}</Styled.Text>
+      </Styled.Container>
+      <Styled.Container>
+        <Styled.Label>Дата народження:</Styled.Label>
+        <Styled.Text>{dob}</Styled.Text>
+      </Styled.Container>
+    </Styled.Grid>
+    <Styled.BtnContainer>
+      <Button type="submit">Редагувати</Button>
+    </Styled.BtnContainer>
+  </>
 );
