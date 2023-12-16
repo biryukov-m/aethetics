bda8fd0977c17812fc686c723342d10575599166
Max <biryukov.mm@gmail.com>
2023-12-16 16:40:42 +0200

    feat: Add tab indication on order page

--- src/modules/order/pages/Order.tsx
+++ src/modules/order/pages/Order.tsx
@@ -1,30 +1,44 @@
 import React from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import * as Styled from './Order.styled';
 import { OrderBasket } from '../components/OrderBasket/OrderBasket';
 import { ROUTER_KEYS } from '../../../constants/routerKeys';
 
 const Order: React.FC = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const currentPath = location.pathname;
 
   return (
-    <Styled.Layout>
-      <div>
+    <section>
+      <Styled.Layout>
         <Styled.Header>Оформлення замовлення</Styled.Header>
         <Styled.Tabs>
           <Styled.Tab onClick={() => navigate(`${ROUTER_KEYS.order.contacts}`)}>
-            <Styled.Counter>1</Styled.Counter>
-            <span>Особисті дані</span>
+            <Styled.Counter
+              $active={currentPath.includes(
+                `${ROUTER_KEYS.order.root}/${ROUTER_KEYS.order.contacts}`
+              )}
+            >
+              1
+            </Styled.Counter>
+            <Styled.Text>Особисті дані</Styled.Text>
           </Styled.Tab>
           <Styled.Tab onClick={() => navigate(`${ROUTER_KEYS.order.delivery}`)}>
-            <Styled.Counter>2</Styled.Counter>
-            <span>Оплата / Доставка</span>
+            <Styled.Counter
+              $active={currentPath.includes(
+                `${ROUTER_KEYS.order.root}/${ROUTER_KEYS.order.delivery}`
+              )}
+            >
+              2
+            </Styled.Counter>
+            <Styled.Text>Оплата / Доставка</Styled.Text>
           </Styled.Tab>
         </Styled.Tabs>
         <Outlet />
         <OrderBasket />
-      </div>
-    </Styled.Layout>
+      </Styled.Layout>
+    </section>
   );
 };
 
