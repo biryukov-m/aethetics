1ce58387cbb3c967a7607a9f20c7f91241b108e1
Max <biryukov.mm@gmail.com>
2023-12-18 12:39:15 +0200

    bug: Fix line endings to LF

---
+++ sanity/schemas/product.ts
@@ -0,0 +1,12 @@
+export default {
+  name: 'product',
+  type: 'document',
+  title: 'Product',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name',
+    },
+  ],
+}
