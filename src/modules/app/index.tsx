9bdde7452a7ca314bbcb5585c81ecd83fa3a34cd
Max <biryukov.mm@gmail.com>
2024-01-02 19:53:39 +0200

    ref: Move query client to common api folder

--- src/modules/app/index.tsx
+++ src/modules/app/index.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { MainRouter } from '../navigation';
 import '../../sass/all.scss';
 import { BasketContextProvider } from '../basket-modal/Basket.provider';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnMount: false,
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: true
-    }
-  }
-});
+import { queryClient } from '../common/api/queryClient';
 
 const AppContainer: React.FC = () => (
   <BasketContextProvider>
